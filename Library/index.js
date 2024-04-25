b6038270160bdd388b6775a8a5f874d5c3b7392d
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-04-25 18:51:45 +0100

    basic functionality done - layout needs finishing

--- Library/index.js
+++ Library/index.js
@@ -1,19 +1,28 @@
 const myLibrary = [];
 
-function Book(author, title, numberOfPages, read) {
+function Book(author, title, numberOfPages) {
   this.author = author;
   this.title = title;
   this.numberOfPages = numberOfPages;
-  this.read = read;
+  this.read = "no";
 }
 
 function addBookToLibrary(author, title, numberOfPages) {
-  const aBook = Object.create(Book(author, title, numberOfPages));
+  const aBook = new Book(author, title, numberOfPages);
   myLibrary.push(aBook);
+  removeElementsFromDefined("article");
+  displayLibrary();
+}
+
+function removeElementsFromDefined(anId){
+    const el = document.getElementById(anId);
+    while(el.firstChild) {
+        el.removeChild(el.lastChild);
+    }
 }
 
 function displayLibrary(){
-    for(let i=0; i < myLibrary.length(); i++){
+    for(let i=0; i < myLibrary.length; i++){
         const div = document.createElement("div");
         div.id = "book" + i;
         div.className = "card";
@@ -28,9 +37,11 @@ function displayLibrary(){
         const bookAuthor = document.createTextNode(myLibrary[i].author);
         document.getElementById(div.id).appendChild(bookAuthor);
 
+        document.getElementById(div.id).appendChild(br.cloneNode());
         const bookPages = document.createTextNode(myLibrary[i].numberOfPages);
         document.getElementById(div.id).appendChild(bookPages);
 
+        document.getElementById(div.id).appendChild(br.cloneNode());
         const bookRead = document.createTextNode(myLibrary[i].read);
         document.getElementById(div.id).appendChild(bookRead);
     }
@@ -42,6 +53,7 @@ function addBookListner() {
 }
 
 function addBookModal() {
+    console.log("here");
     // Get the modal
     var modal = document.getElementById("addBookModal");
 
@@ -82,24 +94,38 @@ function addBookModal() {
     list_feild_div.id = 'list_feild_div';
     document.getElementById('sortContainer').appendChild(list_feild_div); 
 
-    var valueList = ['Work Order', 'Die Number', 'Customer', 'Order Number', 'Start Date', 'End Date', 'On Time']
+    // Create value field for selected option
+    let valueInput = document.createElement('input');
+    valueInput.setAttribute('type', 'text');
+    valueInput.setAttribute('name', 'input');
+    valueInput.setAttribute('placeholder', 'Title');
+    valueInput.id = 'titleInput'; 
+    document.getElementById('sortContainer').appendChild(valueInput);
 
-    var selectList = document.createElement("select");
-    selectList.id = "componentSelect";
-    document.getElementById('list_feild_div').appendChild(selectList);
+    // Create value field for selected option
+    let authorInput = document.createElement('input');
+    authorInput.setAttribute('type', 'text');
+    authorInput.setAttribute('name', 'input');
+    authorInput.setAttribute('placeholder', 'Author');
+    authorInput.id = 'authorInput'; 
+    document.getElementById('sortContainer').appendChild(authorInput);
 
-    for (var i = 0; i < valueList.length; i++) {
-        var option = document.createElement("option");
-        option.value = valueList[i];
-        option.text = valueList[i];
-        selectList.appendChild(option);
-    }
+    // Create value field for selected option
+    let pagesInput = document.createElement('input');
+    pagesInput.setAttribute('type', 'text');
+    pagesInput.setAttribute('name', 'input');
+    pagesInput.setAttribute('placeholder', 'Author');
+    pagesInput.id = 'pagesInput'; 
+    document.getElementById('sortContainer').appendChild(pagesInput);
 
     document.getElementById('sortContainer').appendChild(br.cloneNode());
     let listSearchActionButton = document.createElement('button');
     listSearchActionButton.appendChild(document.createTextNode('Submit'));
-    listSearchActionButton.addEventListener('click', function() {insertDeptToDoElements(document.getElementById('deptSelect').value, document.getElementById('componentSelect').value)});
+    listSearchActionButton.addEventListener('click', function() {addBookToLibrary(document.getElementById('authorInput').value, 
+                                                                                document.getElementById('titleInput').value,
+                                                                                document.getElementById('pagesInput').value)});
     document.getElementById('list_feild_div').appendChild(listSearchActionButton);
+
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
@@ -110,3 +136,5 @@ function addBookModal() {
     }
     } 
 }
+
+addBookListner();
